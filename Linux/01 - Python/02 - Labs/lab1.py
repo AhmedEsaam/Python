f4aee1d1f32c11cc165082286bc08f41c7dfd315
Ahmed Essam <ahmed.e.mogy@gmail.com>
2023-05-03 22:53:57 +0300

    first commit

---
+++ Linux/01 - Python/02 - Labs/lab1.py
@@ -0,0 +1,3 @@
+print ("I'm Ahmed Essam")
+print ("My birth date is Aug, 24")
+print ("I've been graduated from faculty of Engineering, Ain Shams Uni. in 2022")
---
+++ Linux/01 - Python/02 - Labs/lab2.py
@@ -0,0 +1,8 @@
+MyName = input ("Please Enter your Name: ")
+print("My name is " + (MyName))
+
+MyBirthDate = input ("Please Enter your Birth Date: ")
+print("My Birth Date is " + (MyBirthDate))
+
+MyFaculty = input ("Please Enter your Faculty: ")
+print("My Faculty is " + (MyFaculty))

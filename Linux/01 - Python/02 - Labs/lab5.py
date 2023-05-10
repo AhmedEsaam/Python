d76e7297550c34e6e555dc477fbf32d5a97bc45c
Ahmed Essam <ahmed.e.mogy@gmail.com>
2023-05-10 20:06:16 +0300

    lab5

--- Linux/01 - Python/02 - Labs/lab5.py
+++ Linux/01 - Python/02 - Labs/lab5.py
@@ -1,6 +1,6 @@
 my_set = (input("please enter 5 numbers: ").split(','))
 
-num = (input("please enter the your number: "))
+num = (input("please enter your number: "))
 
 if num in my_set:
     print("number exists")
